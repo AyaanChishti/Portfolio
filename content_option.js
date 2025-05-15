b8f14c71ccd82503abb29af8ccfd24402e675bd2
AyaanChishti <101421283+AyaanChishti@users.noreply.github.com>
2025-05-15 16:45:13 +0530

    Add files via upload

---
+++ content_option.js
@@ -0,0 +1,120 @@
+// import CodehereImg from './assets/Codehere.png';
+const logotext = "MOHD AYAAN CHISHTI";
+const meta = {
+    title: "MOHD AYAAN CHISHTI",
+    description: "I’m Mohd Ayaan Chishti frontend devloper,currently working in Meta Scifor",
+};
+
+const introdata = {
+    title: "I’m Mohd Ayaan Chishti",
+    animated: {
+        first: "I love coding",
+        second: "I code cool websites",
+        third: "I develop mobile apps",
+    },
+    description: "I’m Mohd Ayaan Chishti frontend devloper,currently working in Meta Scifor",
+    your_img_url: "https://images.unsplash.com/photo-1514790193030-c89d266d5a9d",
+};
+
+const dataabout = {
+    title: "abit about my self",
+    aboutme: "I am a senior at the Integral University of Lucknow, majoring in computer science and having 1.5 years of self-experience. I have a passion for computer programming and information technology. This passion has given me the ability to develop programs for a multitude of things, including programming languages like HTML, CSS, Javascript, ReactJS, C, C++, Java and databases. I am open to learning new things and getting involved with new projects that can help me learn or apply all that I have learned throughout my university career.",
+};
+const worktimeline = [{
+        jobtitle: "Frontend Intern",
+        where: "Meta Scifor Technologies",
+        date: "February,2025",
+    },
+];
+
+const skills = [{
+        name: "C++",
+        value: 90,
+    },
+    {
+        name: "HTML",
+        value: 95,
+    },
+    {
+        name: "Javascript",
+        value: 95,
+    },
+    {
+        name: "React",
+        value: 98,
+    },
+    {
+        name: "Java",
+        value: 85,
+    },
+    {
+        name: "MySQL",
+        value: 80,
+    },
+    {
+        name: "Python",
+        value: 90,
+    },
+    {
+        name: "CSS",
+        value: 95,
+    },
+];
+
+const services = [{
+        title: "Frontend Intern",
+        description: "Developed and maintained web pages using HTML, CSS, Bootstrap. Implemented interactive features using JavaScript. Build reusable UI components using ReactJS. Ensured responsive design and cross-browser compatibility. Collaborated with backend developers and designers for seamless integration.",
+    },
+];
+
+const dataportfolio = [{
+        img:"images/Codehere.png",
+        description: "Developed a full-stack web application using with Flask serving a REST API with React as the frontend.",
+        link: "https://github.com/AyaanChishti/codehere_v2",
+    },
+    {
+        img: "images/Leadstracker.png",
+        description: "It is a Chrome Extension that can be deployed on the browser. It keeps track of the leads in order to improve the business. It saves the URL of the tabs on the local storage.",
+        link: "https://github.com/AyaanChishti/LeadTracker",
+    },
+    {
+        img: "images/SMS.png",
+        description: "In this project, my primary objective was to develop a robust system capable of accurately differentiating between two distinct categories: spam and ham messages.",
+        link: "https://github.com/AyaanChishti/SMS-Spam-Classifier",
+    },
+    {
+        img: "images/Car.png",
+        description: "In this ambitious endeavor, I embarked on a mission to forecast the prices of automobiles representing a diverse array of 12 top-tier brands.",
+        link: "https://github.com/AyaanChishti/Car-Price-Predictor",
+    },
+];
+
+const contactConfig = {
+    YOUR_EMAIL: "ayaanchishti444.com",
+    YOUR_FONE: "7619812337",
+    description: "Let’s Connect, Whether you have a question, a project in mind, or just want to say hi — feel free to drop a message. I’m always open to discussing new opportunities, collaborations, or creative ideas.",
+    // creat an emailjs.com account 
+    // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
+    YOUR_SERVICE_ID: "service_id",
+    YOUR_TEMPLATE_ID: "template_id",
+    YOUR_USER_ID: "user_id",
+};
+
+const socialprofils = {
+    github: "https://github.com",
+    facebook: "https://facebook.com",
+    linkedin: "https://linkedin.com",
+    twitter: "https://twitter.com",
+};
+export {
+    meta,
+    dataabout,
+    dataportfolio,
+    worktimeline,
+    skills,
+    services,
+    introdata,
+    contactConfig,
+    socialprofils,
+    logotext,
+};
